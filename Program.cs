7b09a37b2e3254d3b18c83c5cd1b6d6a66f24596
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-11-12 10:44:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Welcome
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome The cab invoice Genrator");
+            Console.ReadLine();
+        }
+    }
+}
