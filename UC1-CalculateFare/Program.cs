6e1e105d27bdd2a669a08886a1d3b695aa47d3e5
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-11-12 10:37:07 +0530

    Calculate Fare for cab

---
+++ UC1-CalculateFare/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC1_CalculateFare
+{
+    public class Fare
+    {
+        public double CostPerKm = 10;
+        public int CostPerMinute = 1;
+        public int MinimumFare = 5;
+        public double CalculateFare(double Distance, int Time)
+        {
+            double Fare = (CostPerKm * Distance) + (CostPerMinute * Time);
+            return Math.Max(Fare, MinimumFare);
+        }
+    }
+   internal class main
+   {
+        static void Main(string[] args)
+        {
+          Fare uc1 = new Fare();
+          double fare1 = uc1.CalculateFare(10, 30);
+          Console.WriteLine($"Total Fare for your Journey is :{fare1}");
+          Console.ReadLine();
+        }
+    }
+}
